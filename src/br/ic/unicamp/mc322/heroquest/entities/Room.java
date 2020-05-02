ab14d0e416176702be9e1cedb20d22197775d9e2
pochete <thefemr@gmail.com>
2020-05-02 16:49:25 -0300

    Implements containment check for rooms

--- src/br/ic/unicamp/mc322/heroquest/entities/Room.java
+++ src/br/ic/unicamp/mc322/heroquest/entities/Room.java
@@ -6,11 +6,13 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Room {
-    private final Point universalPosition;
+    private final Point topLeft, bottomRight;
     private final LinkedList<Entity> entities;
+    private boolean visited;
 
-    public Room(Point universalPosition) {
-        this.universalPosition = new Point(universalPosition);
+    public Room(Point topLeft, Point bottomRight) {
+        this.topLeft = new Point(topLeft);
+        this.bottomRight = new Point(bottomRight);
         this.entities = new LinkedList<>();
     }
 
@@ -20,5 +22,10 @@ public class Room {
 
     public void removeEntity(Entity entity) {
         this.entities.removeIf(x -> x.equals(entity));
+    }
+
+    public boolean contains(Point point) {
+        return this.topLeft.getX() <= point.getX() && this.bottomRight.getX() >= point.getX()
+                && this.topLeft.getY() <= point.getY() && this.bottomRight.getY() >= point.getY();
     }
 }
