ce62971d068c55c11647ac66901c38cc4a511898
pochete <thefemr@gmail.com>
2020-05-02 12:26:40 -0300

    Creates list of entities inside a room

--- src/br/ic/unicamp/mc322/heroquest/entities/Room.java
+++ src/br/ic/unicamp/mc322/heroquest/entities/Room.java
@@ -1,4 +1,20 @@
 package br.ic.unicamp.mc322.heroquest.entities;
 
+import br.ic.unicamp.mc322.heroquest.auxiliars.Point;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Room {
+    private final Point universalPosition;
+    private final LinkedList<Entity> entities;
+
+    public Room(Point universalPosition) {
+        this.universalPosition = new Point(universalPosition);
+        this.entities = new LinkedList<>();
+    }
+
+    public void addEntity(Entity entity) {
+        this.entities.add(entity.getCopy());
+    }
 }
