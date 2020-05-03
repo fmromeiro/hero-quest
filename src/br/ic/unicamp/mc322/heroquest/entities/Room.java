5df099b076b47e65c72bce479d1c376328246c43
pochete <thefemr@gmail.com>
2020-05-02 21:37:10 -0300

    Enhances entity control in Room and implements hasHero method

--- src/br/ic/unicamp/mc322/heroquest/entities/Room.java
+++ src/br/ic/unicamp/mc322/heroquest/entities/Room.java
@@ -2,27 +2,40 @@ package br.ic.unicamp.mc322.heroquest.entities;
 
 import br.ic.unicamp.mc322.heroquest.auxiliars.Point;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 public class Room {
     private final Point topLeft, bottomRight;
-    private final LinkedList<Entity> entities;
+    private static int nextId = 0;
+    private final HashMap<Integer, Entity> entities;
     private boolean visited;
 
     public Room(Point topLeft, Point bottomRight) {
         this.topLeft = new Point(topLeft);
         this.bottomRight = new Point(bottomRight);
-        this.entities = new LinkedList<>();
+        this.entities = new HashMap<>();
     }
 
-    public void addEntity(Entity entity) {
-        this.entities.add(entity.getCopy());
+    public int addEntity(Entity entity) {
+        this.entities.put(nextId, entity.getCopy());
+        return this.nextId++;
     }
 
-    public void removeEntity(Entity entity) {
-        this.entities.removeIf(x -> x.equals(entity));
+    public Entity removeEntity(int id) {
+        return this.entities.remove(id);
     }
+
+    public Entity removeEntity(Entity entity) {
+        for (int id : this.entities.keySet())
+            if (this.entities.get(id).equals(entity))
+                return this.removeEntity(id);
+        return null;
+    }
+
+    private boolean hasHero() { return this.entities.values().stream().anyMatch(x -> x instanceof Hero); }
 
     public boolean contains(Point point) {
         return this.topLeft.getX() <= point.getX() && this.bottomRight.getX() >= point.getX()
